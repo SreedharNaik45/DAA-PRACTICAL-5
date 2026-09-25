355744d9da39fe45fb6e095e421955cc3c7d2595
Sreedhar Naik <sreedharnaiksugali45@gmail.com>
2026-09-25 15:38:23 +0530

    Add 0/1 Knapsack problem implementation
    Implement 0/1 Knapsack problem using dynamic programming.

---
+++ Knapsack.py
@@ -0,0 +1,14 @@
+n = int(input("Enter number of items: "))
+weights = list(map(int, input("Enter weights: ").split()))
+values = list(map(int, input("Enter values: ").split()))
+W = int(input("Enter knapsack capacity: "))
+dp = [[0] * (W + 1) for _ in range(n + 1)]
+for i in range(1, n + 1):
+    for w in range(W + 1):
+        if weights[i - 1] <= w:
+            dp[i][w] = max(
+                values[i - 1] + dp[i - 1][w - weights[i - 1]],
+                dp[i - 1][w])
+        else:
+            dp[i][w] = dp[i - 1][w]
+print("Maximum value:", dp[n][W])
